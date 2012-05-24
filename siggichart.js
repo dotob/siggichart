fda0ef35a70c507eeeda67c0be1804beb7653a47
basti <sk@dotob.de>
2012-05-24 09:25:01 +0200

    convert to kmh

--- siggichart.js
+++ siggichart.js
@@ -5,10 +5,14 @@
   $ = jQuery;
 
   $(document).ready(function() {
-    var loaddata;
+    var convertToKmH, loaddata;
     $("#data").change(function() {
       return loaddata(this);
     });
+    convertToKmH = function(ms) {
+      var kmh;
+      return kmh = ms * 60 * 60 / 1000;
+    };
     return loaddata = function(x) {
       var g, i, logEntries, max, min, min_max, speed, status_bar, xml, xmlDoc;
       xml = $.parseXML($(x).val());
@@ -16,9 +20,8 @@
       i = 0;
       speed = new Array;
       logEntries = xmlDoc.find("LogEntry").each(function() {
-        return speed[i++] = parseInt($(this).find("Speed").text());
+        return speed[i++] = convertToKmH(parseFloat($(this).find("Speed").text()));
       });
-      alert(speed.length);
       min_max = Ico.series_min_max([speed]);
       min = min_max[0];
       max = min_max[1];
@@ -39,10 +42,10 @@
         value_labels: {
           add_padding: false
         },
-        units: 'm/s',
-        units_position: 0,
+        units: 'km/h',
+        units_position: 100,
         x_padding_right: 15,
-        colors: ['#228899', '#339933', 'grey'],
+        colors: ['#228899'],
         mouseover_attributes: {
           stroke: 'green'
         },
--- src/main.coffee
+++ src/main.coffee
@@ -4,14 +4,16 @@ $(document).ready ->
 	$("#data").change ->
 		loaddata this
 		
+	convertToKmH = (ms) ->
+		kmh = ms * 60 * 60 /1000 
+		
 	loaddata = (x) ->
 		xml = $.parseXML($(x).val())
 		xmlDoc = $(xml)
 		i = 0
 		speed = new Array
 		logEntries = xmlDoc.find("LogEntry").each ->
-			speed[i++] = parseInt($(this).find("Speed").text())
-		alert speed.length
+			speed[i++] = convertToKmH parseFloat $(this).find("Speed").text()
 		min_max = Ico.series_min_max( [speed] );
 		min = min_max[0];
 		max = min_max[1];
@@ -30,10 +32,10 @@ $(document).ready ->
 			, font_size: 12
 			, series_names: ['one']
 			, value_labels: { add_padding: false }
-			, units: 'm/s'
-			, units_position: 0
+			, units: 'km/h'
+			, units_position: 100
 			, x_padding_right: 15
-			, colors: ['#228899', '#339933', 'grey']
+			, colors: ['#228899']
 			, mouseover_attributes: { stroke: 'green' }
 			, curve_amount: 3
 			, mouse_pointer: true
